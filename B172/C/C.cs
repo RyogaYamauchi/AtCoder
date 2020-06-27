e1386bc84807ddc82fbed073b7156d64da1315c4
Ryoga Yamauchi <g1854003@gmail.com>
2020-06-27 20:59:38 +0900

    create B172

---
+++ B172/C/C.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace AtCoder.XXX.C
+{ 
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+
+        private static StreamReader sr;
+        public static int[] Ecr()
+        {
+            if (sr == null)
+            {
+                sr = new StreamReader("input.txt", Encoding.GetEncoding("UTF-8"));
+            }
+            return sr.ReadLine().Split().Select(int.Parse).ToArray();
+        }
+        public static  int[] cr()
+        {
+            return Console.ReadLine().Split().Select(int.Parse).ToArray();
+        }
+        public static string Er()
+        {
+            if (sr == null)
+            {
+                sr = new StreamReader("input.txt", Encoding.GetEncoding("UTF-8"));
+            }
+            return sr.ReadLine();
+        }
+        public static string r()
+        {
+            return Console.ReadLine();
+        }
+        public static int cri()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+        public static void P(string str)
+        {
+            Console.WriteLine(str);
+        }
+        public static void P(int num)
+        {
+            P(num.ToString());
+        }
+    }
+}
